e4da1ce48a887306b0487fa3a8e955001eb0caac
selimseker <selim.seker00@gmail.com>
2024-07-25 14:33:31 +0300

    add --trace log-level mode

--- start.sh
+++ start.sh
@@ -34,6 +34,7 @@ fi
 START_MODE="FOREGROUND"
 LOCAL_OLLAMA=true
 LOGS="info"
+DKN_LOG_LEVEL="none,dkn_compute=info" # default info logs
 
 # script internal
 COMPOSE_PROFILES=()
@@ -56,6 +57,9 @@ while [[ "$#" -gt 0 ]]; do
 
         --dev)
             DKN_LOG_LEVEL="none,dkn_compute=debug"
+        ;;
+        --trace)
+            DKN_LOG_LEVEL="none,dkn_compute=trace"
         ;;
         -b|--background) START_MODE="BACKGROUND" ;;
         -h|--help) docs ;;
