9ecf9f0d17218ff275c65970b2a3177df5a179ca
Selim <selim.seker00@gmail.com>
2024-09-02 16:46:53 +0300

    ollama env improvments

--- start/start.go
+++ start/start.go
@@ -77,7 +77,7 @@ func checkDockerComposeCommand() (string, []string, []string) {
 	// both not found, exit
 	fmt.Println("docker compose is not installed on this machine. It's required to run the node.")
 	fmt.Println("Check https://docs.docker.com/compose/install/ for installation.")
-	// os.Exit(1)
+	exitWithDelay(1)
 	return "", nil, nil
 }
 
@@ -93,7 +93,7 @@ func checkRequiredEnvVars(envvars map[string]string) {
 		skey, err := getDknSecretKey()
 		if err != nil {
 			fmt.Printf("Error during user input: %s\n", err)
-			os.Exit(1)
+			exitWithDelay(1)
 		}
 		envvars["DKN_WALLET_SECRET_KEY"] = skey
 		absent_env = true
@@ -115,7 +115,7 @@ func setWorkingDir() {
 	ex, err := os.Executable()
 	if err != nil {
 		fmt.Printf("Error during getting the working directory %s\n", err)
-		os.Exit(1)
+		exitWithDelay(1)
 	}
 	WORKING_DIR = filepath.Dir(ex)
 }
@@ -186,6 +186,100 @@ func runOllamaServe(host, port string) (int, error) {
 	return pid, fmt.Errorf("ollama failed to start after %d retries", OLLAMA_MAX_RETRIES)
 }
 
+func handleOllamaEnv(ollamaHost, ollamaPort string, dockerOllama bool) (string, string, string, string) {
+	// local ollama
+	if !dockerOllama {
+		if isCommandAvailable("ollama") {
+			// host machine has ollama installed
+			// we first going to check whether its serving or not
+			// if not script runs ollama serve command manually and stores its pid
+
+			// prepare local ollama url
+			if ollamaHost == "" || ollamaHost == DOCKER_HOST {
+				// we have to check Ollama at host, but if the given host is
+				// host.docker.internal we still have to check the localhost
+				ollamaHost = LOCAL_HOST
+			}
+			if ollamaPort == "" {
+				ollamaPort = strconv.Itoa(DEFAULT_OLLAMA_PORT)
+			}
+
+			// check is it already serving
+			if isOllamaServing(ollamaHost, ollamaPort) {
+				fmt.Printf("Local Ollama is already up at %s:%s and running, using it\n", ollamaHost, ollamaPort)
+			} else {
+				// ollama is not live, so we launch it ourselves
+				fmt.Println("Local Ollama is not live, running ollama serve")
+				ollama_pid, err := runOllamaServe(ollamaHost, ollamaPort)
+				if err != nil {
+					// ollama failed to start, exit
+					fmt.Println(err)
+					fmt.Println("You can use the --docker-ollama flag to use the Docker Ollama image instead.")
+					exitWithDelay(1)
+				} else {
+					fmt.Printf("Local Ollama server is up at %s:%s and running with PID %d\n", ollamaHost, ollamaPort, ollama_pid)
+				}
+			}
+
+			// to use the local Ollama, we need to configure the network depending on the Host
+			// Windows and Mac should work with host.docker.internal alright,
+			// but Linux requires `host` network mode with `localhost` as the Host URL
+			if runtime.GOOS == "darwin" {
+				ollamaHost = DOCKER_HOST
+			} else if runtime.GOOS == "windows" {
+				ollamaHost = DOCKER_HOST
+			} else if runtime.GOOS == "linux" {
+				ollamaHost = LOCAL_HOST
+			}
+		} else {
+			// although --docker-ollama was not passed, we checked and couldnt find Ollama
+			// so we will use Docker anyways
+			fmt.Println("Ollama is not installed on this machine, will use Docker Ollama service.")
+			dockerOllama = true
+		}
+	}
+
+	composeProfile := ""
+	if dockerOllama {
+		// using docker-ollama, check profiles
+		if isCommandAvailable("nvidia-smi") {
+			composeProfile = "ollama-cuda"
+			fmt.Println("GPU type detected: CUDA")
+		} else if isCommandAvailable("rocminfo") {
+			fmt.Println("GPU type detected: ROCM")
+			composeProfile = "ollama-rocm"
+		} else {
+			fmt.Println("No GPU found, using ollama-cpu")
+			composeProfile = "ollama-cpu"
+		}
+
+		// since docker-ollama is using, set docker.internal for the Ollama host
+		ollamaHost = DOCKER_HOST
+		ollamaPort = strconv.Itoa(DEFAULT_OLLAMA_PORT)
+	}
+
+	// depending on the OS, use host or bridge network modes
+	// https://docs.docker.com/engine/network/#drivers
+	dockerNetworkMode := ""
+	if runtime.GOOS == "darwin" {
+		dockerNetworkMode = "bridge"
+	} else if runtime.GOOS == "windows"  {
+		dockerNetworkMode = "bridge"
+	} else if runtime.GOOS == "linux" {
+		dockerNetworkMode = "host"
+	} 
+
+	return ollamaHost, ollamaPort, dockerNetworkMode, composeProfile
+}
+
+func formatMapKeys(m map[string]bool) string {
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+	return "[" + strings.Join(keys, ", ") + "]"
+}
+
 func pickModels() string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Please pick the model you want to run:\n\n")
@@ -241,11 +335,7 @@ func pickModels() string {
 		}
 	}
 	if len(invalid_selections) != 0 {
-		fmt.Printf("Invalid selections: ")
-		for k := range invalid_selections {
-			fmt.Printf("%s, ", k)
-		}
-		fmt.Printf(" skipping them\n")
+		fmt.Printf("Skipping the invalid selections: %s \n", formatMapKeys(invalid_selections))
 	}
 	fmt.Printf("\n")
 	return picked_models_str
@@ -264,7 +354,12 @@ func getUserInput(message string, trim bool) string {
 		answer = strings.ReplaceAll(answer, " ", "")
 	}
 	return answer
+}
 
+func exitWithDelay(code int) {
+	fmt.Println("Terminating in 5 seconds...")
+	time.Sleep(5 * time.Second)
+	os.Exit(code)
 }
 
 type ModelList []string
@@ -283,20 +378,21 @@ func (models *ModelList) Set(value string) error {
 }
 
 var (
-	OLLAMA_MODELS       = []string{"adrienbrault/nous-hermes2theta-llama3-8b:q8_0", "phi3:14b-medium-4k-instruct-q4_1", "phi3:14b-medium-128k-instruct-q4_1", "phi3:3.8b", "phi3.5:3.8b", "phi3.5:3.8b-mini-instruct-fp16", "llama3.1:latest"}
+	OLLAMA_MODELS       = []string{
+		"adrienbrault/nous-hermes2theta-llama3-8b:q8_0",
+		"phi3:14b-medium-4k-instruct-q4_1",
+		"phi3:14b-medium-128k-instruct-q4_1",
+		"phi3:3.8b",
+		"phi3.5:3.8b",
+		"phi3.5:3.8b-mini-instruct-fp16",
+		"llama3.1:latest",
+		"llama3.1:8b-instruct-q8_0",
+	}
 	OPENAI_MODELS       = []string{"gpt-3.5-turbo","gpt-4-turbo","gpt-4o","gpt-4o-mini"}
 	DEFAULT_OLLAMA_PORT = 11434
-	OLLAMA_REQUIRED     = false
-	OPENAI_REQUIRED     = false
 	DOCKER_HOST         = "http://host.docker.internal"
 	LOCAL_HOST          = "http://localhost"
 	OLLAMA_MAX_RETRIES  = 5
-	COMPOSE_PROFILES    = []string{}
-
-	// this is the default network mode, but
-	// based on local Ollama & OS we may set it to `host`
-	// https://docs.docker.com/engine/network/#drivers
-	DKN_DOCKER_NETWORK_MODE = "bridge"
 
 	// Default admin public key, it will be used unless --dkn-admin-public-key is given
 	DKN_ADMIN_PUBLIC_KEY = "0208ef5e65a9c656a6f92fb2c770d5d5e2ecffe02a6aade19207f75110be6ae658"
@@ -318,6 +414,8 @@ func main() {
 	trace := flag.Bool("trace", false, "Sets the logging level to trace (default: false)")
 	dockerOllama := flag.Bool("docker-ollama", false, "Indicates the Ollama docker image is being used (default: false)")
 	dkn_admin_pkey_flag := flag.String("dkn-admin-public-key", DKN_ADMIN_PUBLIC_KEY, "DKN Admin Node Public Key, usually dont need this since it's given by default")
+	pick_model := flag.Bool("pick-models", false, "Pick the models using cli, supprases the -m flags (default: false)")
+
 	flag.Parse()
 	// override DKN_ADMIN_PUBLIC_KEY if flag is a different value
 	DKN_ADMIN_PUBLIC_KEY = *dkn_admin_pkey_flag
@@ -336,8 +434,8 @@ func main() {
 	// Check Docker Compose
 	composeCommand, composeUpArgs, composeDownArgs := checkDockerComposeCommand()
 	if !isDockerUp() {
-		fmt.Println("ERROR: Docker is not up, terminating.")
-		os.Exit(1)
+		fmt.Println("ERROR: Docker is not up")
+		exitWithDelay(1)
 	}
 
 	// first load .env file if exists
@@ -346,8 +444,8 @@ func main() {
 		// if couldnt load the .env, use .env.example
 		envvars, err = godotenv.Read(filepath.Join(WORKING_DIR, ".env.example"))
 		if err != nil {
-			fmt.Println("Couldn't locate both .env and .env.example, terminating...")
-			os.Exit(1)
+			fmt.Println("Couldn't locate both .env and .env.example")
+			exitWithDelay(1)
 		}
 	}
 
@@ -359,11 +457,11 @@ func main() {
 	}
 
 	// if DKN_MODELS are still empty, pick model interactively
-	if envvars["DKN_MODELS"] == "" {
+	if envvars["DKN_MODELS"] == "" || *pick_model {
 		pickedModels := pickModels()
 		if pickedModels == "" {
-			fmt.Println("No valid model picked, terminating...")
-			os.Exit(1)
+			fmt.Println("No valid model picked")
+			exitWithDelay(1)
 		}
 		envvars["DKN_MODELS"] = pickedModels
 	}
@@ -375,8 +473,8 @@ func main() {
 				if envvars["OPENAI_API_KEY"] == "" {
 					apikey := getUserInput("Enter your OpenAI API Key", true)
 					if apikey == "" {
-						fmt.Printf("Invalid input, please place your OPENAI_API_KEY to .env file, terminating.\n")
-						os.Exit(1)
+						fmt.Printf("Invalid input, please place your OPENAI_API_KEY to .env file\n")
+						exitWithDelay(1)
 					}
 					envvars["OPENAI_API_KEY"] = apikey
 				}
@@ -391,6 +489,7 @@ func main() {
 	fmt.Printf("\n")
 
 	// check ollama requirement
+	OLLAMA_REQUIRED := false
 	for _, model := range strings.Split(envvars["DKN_MODELS"], ",") {
 		for _, ollama_model := range OLLAMA_MODELS {
 			if model == ollama_model {
@@ -402,81 +501,13 @@ func main() {
 
 	// check ollama environment
 	if OLLAMA_REQUIRED {
-		// local ollama
-		if !(*dockerOllama) {
-			if isCommandAvailable("ollama") {
-				// host machine has ollama installed
-				// we first going to check whether its serving or not
-				// if not script runs ollama serve command manually and stores its pid
-
-				// prepare local ollama url
-				if envvars["OLLAMA_HOST"] == "" || envvars["OLLAMA_HOST"] == DOCKER_HOST {
-					// we have to check Ollama at host, but if the given host is
-					// host.docker.internal we still have to check the localhost
-					// here, we construct `ollama_url` with respect to that
-					envvars["OLLAMA_HOST"] = LOCAL_HOST
-				}
-				if envvars["OLLAMA_PORT"] == "" {
-					envvars["OLLAMA_PORT"] = strconv.Itoa(DEFAULT_OLLAMA_PORT)
-				}
-
-				// check is it already serving
-				if isOllamaServing(envvars["OLLAMA_HOST"], envvars["OLLAMA_PORT"]) {
-					fmt.Printf("Local Ollama is already up at %s:%s and running, using it\n", envvars["OLLAMA_HOST"], envvars["OLLAMA_PORT"])
-				} else {
-					// ollama is not live, so we launch it ourselves
-					fmt.Println("Local Ollama is not live, running ollama serve")
-					ollama_pid, err := runOllamaServe(envvars["OLLAMA_HOST"], envvars["OLLAMA_PORT"])
-					if err != nil {
-						// ollama failed to start, exit
-						fmt.Println(err)
-						fmt.Println("You can use the --docker-ollama flag to use the Docker Ollama image instead.")
-						os.Exit(1)
-					} else {
-						fmt.Printf("Local Ollama server is up at %s:%s and running with PID %d\n", envvars["OLLAMA_HOST"], envvars["OLLAMA_PORT"], ollama_pid)
-					}
-				}
-
-				// to use the local Ollama, we need to configure the network depending on the Host
-				// Windows and Mac should work with host.docker.internal alright,
-				// but Linux requires `host` network mode with `localhost` as the Host URL
-				if runtime.GOOS == "darwin" {
-					envvars["OLLAMA_HOST"] = DOCKER_HOST
-				} else if runtime.GOOS == "linux" {
-					envvars["OLLAMA_HOST"] = LOCAL_HOST
-					DKN_DOCKER_NETWORK_MODE = "host"
-					// } else if runtime.GOOS == "windows" {
-					// 	// TODO test for windows
-					// 	envvars["OLLAMA_HOST"] = LOCAL_HOST
-				}
-			} else {
-				// although --docker-ollama was not passed, we checked and couldnt find Ollama
-				// so we will use Docker anyways
-				fmt.Println("Ollama is not installed on this machine, will use Docker Ollama service.")
-				*dockerOllama = true
-			}
-		}
-
-		if *dockerOllama {
-			// using docker-ollama, check profiles
-			if isCommandAvailable("nvidia-smi") {
-				COMPOSE_PROFILES = append(COMPOSE_PROFILES, "ollama-cuda")
-				fmt.Println("GPU type detected: CUDA")
-			} else if isCommandAvailable("rocminfo") {
-				fmt.Println("GPU type detected: ROCM")
-				COMPOSE_PROFILES = append(COMPOSE_PROFILES, "ollama-rocm")
-			} else {
-				fmt.Println("No GPU found, using ollama-cpu")
-				COMPOSE_PROFILES = append(COMPOSE_PROFILES, "ollama-cpu")
-			}
-
-			// use docker internal for the Ollama host
-			envvars["OLLAMA_HOST"] = DOCKER_HOST
-			envvars["OLLAMA_PORT"] = strconv.Itoa(DEFAULT_OLLAMA_PORT)
-			DKN_DOCKER_NETWORK_MODE = "bridge"
-		}
-
-		fmt.Printf("Ollama host: %s (network mode: %s)\n", envvars["OLLAMA_HOST"], DKN_DOCKER_NETWORK_MODE)
+		ollamaHost, ollamaPort, dockerNetworkMode, composeProfile := handleOllamaEnv(envvars["OLLAMA_HOST"], envvars["OLLAMA_PORT"], *dockerOllama)
+		envvars["OLLAMA_HOST"] = ollamaHost
+		envvars["OLLAMA_PORT"] = ollamaPort
+		envvars["COMPOSE_PROFILES"] = composeProfile
+		envvars["DKN_DOCKER_NETWORK_MODE"] = dockerNetworkMode
+	
+		fmt.Printf("Ollama host: %s (network mode: %s)\n", envvars["OLLAMA_HOST"], envvars["DKN_DOCKER_NETWORK_MODE"])
 	} else {
 		fmt.Println("No Ollama model provided. Skipping the Ollama execution")
 	}
@@ -496,15 +527,11 @@ func main() {
 	_, err = runCommand(true, true, []string{"DOCKER_CLI_HINTS=false"}, "docker", "pull", "firstbatch/dkn-compute-node:latest")
 	if err != nil {
 		fmt.Println("Error during pulling the latest compute node image")
-		os.Exit(1)
+		exitWithDelay(1)
 	}
 
 	// dump the final env
 	godotenv.Write(envvars, filepath.Join(WORKING_DIR, ".env"))
-
-	// set runtime env vars
-	envvars["COMPOSE_PROFILES"] = strings.Join(COMPOSE_PROFILES, ",")
-	envvars["DKN_DOCKER_NETWORK_MODE"] = DKN_DOCKER_NETWORK_MODE
 
 	if *background {
 		fmt.Printf("\nStarting in BACKGROUND mode...\n")
@@ -520,7 +547,7 @@ func main() {
 	_, err = runCommand(true, true, mapToList(envvars), composeCommand, composeUpArgs...)
 	if err != nil {
 		fmt.Printf("ERROR: docker-compose, %s", err)
-		os.Exit(1)
+		exitWithDelay(1)
 	}
 
 	fmt.Println("All good! Compute node is up and running.")
