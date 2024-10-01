6de5628fa62e869aeed1425da0c200656f15c872
erhant <erhany96@gmail.com>
2024-10-01 18:17:06 +0300

    removed start.sh, bump minor version

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Start script is deprecated, please see: https://dria.co/guide"
-echo "We are using a cross-platform launcher now: https://github.com/firstbatchxyz/dkn-compute-launcher"
-echo "After a few releases, this script will be removed all-together."
-echo "Exiting..."
-exit 1
