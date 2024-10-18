d18710ce75f3dc459df969bb1988073bb0c1706c
erhant <erhany96@gmail.com>
2024-10-18 15:51:15 +0300

    feat: Gemini (#135)
    * gemini setup for api check
    
    * docs and tests, todo small fix
    
    * fixed api checks, better tests
    
    * bump version

--- workflows/src/providers/gemini.rs
+++ workflows/src/providers/gemini.rs
@@ -6,37 +6,43 @@ use std::env;
 
 use crate::utils::safe_read_env;
 
-const OPENAI_MODELS_API: &str = "https://api.openai.com/v1/models";
-const ENV_VAR_NAME: &str = "OPENAI_API_KEY";
+/// [`models.list`](https://ai.google.dev/api/models#method:-models.list) endpoint
+const GEMINI_MODELS_API: &str = "https://generativelanguage.googleapis.com/v1beta/models";
+const ENV_VAR_NAME: &str = "GEMINI_API_KEY";
 
-/// [Model](https://platform.openai.com/docs/api-reference/models/object) API object, fields omitted.
+/// [Model](https://ai.google.dev/api/models#Model) API object, fields omitted.
 #[derive(Debug, Clone, Deserialize)]
-struct OpenAIModel {
-    /// The model identifier, which can be referenced in the API endpoints.
-    id: String,
+#[allow(non_snake_case)]
+#[allow(unused)]
+struct GeminiModel {
+    name: String,
+    version: String,
+    // other fields are ignored from API response
 }
 
 #[derive(Debug, Clone, Deserialize)]
-struct OpenAIModelsResponse {
-    data: Vec<OpenAIModel>,
+#[allow(non_snake_case)]
+#[allow(unused)]
+struct GeminiModelsResponse {
+    models: Vec<GeminiModel>,
 }
 
 /// OpenAI-specific configurations.
 #[derive(Debug, Clone, Default)]
-pub struct OpenAIConfig {
+pub struct GeminiConfig {
     /// API key, if available.
     api_key: Option<String>,
 }
 
-impl OpenAIConfig {
-    /// Looks at the environment variables for OpenAI API key.
+impl GeminiConfig {
+    /// Looks at the environment variables for Gemini API key.
     pub fn new() -> Self {
         Self {
             api_key: safe_read_env(env::var(ENV_VAR_NAME)),
         }
     }
 
-    /// Sets the API key for OpenAI.
+    /// Sets the API key for Gemini.
     pub fn with_api_key(mut self, api_key: String) -> Self {
         self.api_key = Some(api_key);
         self
@@ -44,18 +50,18 @@ impl OpenAIConfig {
 
     /// Check if requested models exist & are available in the OpenAI account.
     pub async fn check(&self, models: Vec<Model>) -> Result<Vec<Model>> {
-        log::info!("Checking OpenAI requirements");
+        log::info!("Checking Gemini requirements");
 
         // check API key
         let Some(api_key) = &self.api_key else {
-            return Err(eyre!("OpenAI API key not found"));
+            return Err(eyre!("Gemini API key not found"));
         };
 
         // fetch models
         let client = Client::new();
         let request = client
-            .get(OPENAI_MODELS_API)
-            .header("Authorization", format!("Bearer {}", api_key))
+            .get(GEMINI_MODELS_API)
+            .query(&[("key", api_key)])
             .build()
             .wrap_err("failed to build request")?;
 
@@ -67,22 +73,26 @@ impl OpenAIConfig {
         // parse response
         if response.status().is_client_error() {
             return Err(eyre!(
-                "Failed to fetch OpenAI models:\n{}",
+                "Failed to fetch Gemini models:\n{}",
                 response.text().await.unwrap_or_default()
             ));
         }
-        let openai_models = response.json::<OpenAIModelsResponse>().await?;
+        let gemini_models = response.json::<GeminiModelsResponse>().await?;
 
         // check if models exist and select those that are available
         let mut available_models = Vec::new();
         for requested_model in models {
-            if !openai_models
-                .data
-                .iter()
-                .any(|m| m.id == requested_model.to_string())
-            {
+            if !gemini_models.models.iter().any(|gemini_model| {
+                // a gemini model name in API response is given as `models/{baseModelId}-{version}`
+                // the model name in Workflows can include the version as well, so best bet is to check prefix
+                // ignoring the `models/` part
+                gemini_model
+                    .name
+                    .trim_start_matches("models/")
+                    .starts_with(&requested_model.to_string())
+            }) {
                 log::warn!(
-                    "Model {} not found in your OpenAI account, ignoring it.",
+                    "Model {} not found in your Gemini account, ignoring it.",
                     requested_model
                 );
             } else {
@@ -91,9 +101,10 @@ impl OpenAIConfig {
         }
 
         log::info!(
-            "OpenAI checks are finished, using models: {:#?}",
+            "Gemini checks are finished, using models: {:#?}",
             available_models
         );
+
         Ok(available_models)
     }
 }
@@ -103,24 +114,26 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    #[ignore = "requires OpenAI API key"]
-    async fn test_openai_check() {
+    #[ignore = "requires Gemini API key"]
+    async fn test_gemini_check() {
         let _ = dotenvy::dotenv(); // read api key
         assert!(env::var(ENV_VAR_NAME).is_ok(), "should have api key");
 
-        let models = vec![Model::GPT4Turbo, Model::GPT4o, Model::GPT4oMini];
-        let config = OpenAIConfig::new();
-        let res = config.check(models.clone()).await;
+        let models = vec![
+            Model::Gemini10Pro,
+            Model::Gemini15ProExp0827,
+            Model::Gemini15Flash,
+            Model::Gemini15Pro,
+        ];
+        let res = GeminiConfig::new().check(models.clone()).await;
         assert_eq!(res.unwrap(), models);
 
         env::set_var(ENV_VAR_NAME, "i-dont-work");
-        let config = OpenAIConfig::new();
-        let res = config.check(vec![]).await;
+        let res = GeminiConfig::new().check(vec![]).await;
         assert!(res.is_err());
 
         env::remove_var(ENV_VAR_NAME);
-        let config = OpenAIConfig::new();
-        let res = config.check(vec![]).await;
+        let res = GeminiConfig::new().check(vec![]).await;
         assert!(res.is_err());
     }
 }
