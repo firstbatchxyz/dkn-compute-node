285995e96fc70cbac7d0484f608c9891edbc5bf9
erhant <erhany96@gmail.com>
2024-10-21 10:19:06 +0300

    report tps init

---
+++ workflows/src/bin/tps.rs
@@ -0,0 +1,9 @@
+#[tokio::main]
+async fn main() {
+    ///////
+    // loop over all Ollama models
+    // report TPS to console
+
+    ///////
+    // report machine info here as well, such as OS / CPU / RAM / Ollama version
+}
