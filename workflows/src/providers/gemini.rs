84675ad9af8f88edcd50dfbca830ce885a045b28
erhant <erhany96@gmail.com>
2024-10-30 13:50:26 +0300

    added dummy gen tests for openai and gemini

--- workflows/src/providers/gemini.rs
+++ workflows/src/providers/gemini.rs
@@ -6,26 +6,7 @@ use std::env;
 
 use crate::utils::safe_read_env;
 
-/// [`models.list`](https://ai.google.dev/api/models#method:-models.list) endpoint
-const GEMINI_MODELS_API: &str = "https://generativelanguage.googleapis.com/v1beta/models";
 const ENV_VAR_NAME: &str = "GEMINI_API_KEY";
-
-/// [Model](https://ai.google.dev/api/models#Model) API object, fields omitted.
-#[derive(Debug, Clone, Deserialize)]
-#[allow(non_snake_case)]
-#[allow(unused)]
-struct GeminiModel {
-    name: String,
-    version: String,
-    // other fields are ignored from API response
-}
-
-#[derive(Debug, Clone, Deserialize)]
-#[allow(non_snake_case)]
-#[allow(unused)]
-struct GeminiModelsResponse {
-    models: Vec<GeminiModel>,
-}
 
 /// OpenAI-specific configurations.
 #[derive(Debug, Clone, Default)]
@@ -57,10 +38,76 @@ impl GeminiConfig {
             return Err(eyre!("Gemini API key not found"));
         };
 
+        // check if models exist and select those that are available
+        let gemini_models_names = self.fetch_models(api_key).await?;
+        let mut available_models = Vec::new();
+        for requested_model in models {
+            // check if model exists
+            if !gemini_models_names
+                .iter()
+                .any(|model| model.starts_with(&requested_model.to_string()))
+            {
+                log::warn!(
+                    "Model {} not found in your Gemini account, ignoring it.",
+                    requested_model
+                );
+                continue;
+            }
+
+            // make a dummy request
+            if let Err(err) = self.dummy_request(&api_key, &requested_model).await {
+                log::warn!(
+                    "Model {} failed dummy request, ignoring it: {}",
+                    requested_model,
+                    err
+                );
+                continue;
+            }
+
+            available_models.push(requested_model);
+        }
+
+        // log results
+        if available_models.is_empty() {
+            log::warn!("Gemini checks are finished, no available models found.",);
+        } else {
+            log::info!(
+                "Gemini checks are finished, using models: {:#?}",
+                available_models
+            );
+        }
+
+        Ok(available_models)
+    }
+
+    /// Returns the list of models available to this account.
+    ///
+    /// A gemini model name in API response is given as `models/{baseModelId}-{version}`
+    /// the model name in Workflows can include the version as well, so best bet is to check prefix
+    /// ignoring the `models/` part.
+    async fn fetch_models(&self, api_key: &str) -> Result<Vec<String>> {
+        /// [Model](https://ai.google.dev/api/models#Model) API object, fields omitted.
+        #[derive(Debug, Clone, Deserialize)]
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        struct GeminiModel {
+            name: String,
+            version: String,
+            // other fields are ignored from API response
+        }
+
+        #[derive(Debug, Clone, Deserialize)]
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        struct GeminiModelsResponse {
+            models: Vec<GeminiModel>,
+        }
+
         // fetch models
         let client = Client::new();
         let request = client
-            .get(GEMINI_MODELS_API)
+            // [`models.list`](https://ai.google.dev/api/models#method:-models.list) endpoint
+            .get("https://generativelanguage.googleapis.com/v1beta/models")
             .query(&[("key", api_key)])
             .build()
             .wrap_err("failed to build request")?;
@@ -79,33 +126,52 @@ impl GeminiConfig {
         }
         let gemini_models = response.json::<GeminiModelsResponse>().await?;
 
-        // check if models exist and select those that are available
-        let mut available_models = Vec::new();
-        for requested_model in models {
-            if !gemini_models.models.iter().any(|gemini_model| {
-                // a gemini model name in API response is given as `models/{baseModelId}-{version}`
-                // the model name in Workflows can include the version as well, so best bet is to check prefix
-                // ignoring the `models/` part
-                gemini_model
-                    .name
-                    .trim_start_matches("models/")
-                    .starts_with(&requested_model.to_string())
-            }) {
-                log::warn!(
-                    "Model {} not found in your Gemini account, ignoring it.",
-                    requested_model
-                );
-            } else {
-                available_models.push(requested_model);
-            }
+        Ok(gemini_models
+            .models
+            .into_iter()
+            .map(|model| model.name.trim_start_matches("models/").to_string())
+            .collect())
+    }
+
+    async fn dummy_request(&self, api_key: &str, model: &Model) -> Result<()> {
+        log::debug!("Making a dummy request with: {}", model);
+        let client = Client::new();
+        let request = client
+            .post(format!(
+                "https://generativelanguage.googleapis.com/v1beta/models/{}:generateContent",
+                model.to_string()
+            ))
+            .query(&[("key", api_key)])
+            .header("Content-Type", "application/json")
+            .body(
+                serde_json::json!({
+                 "contents": [{
+                   "parts":[{"text": "What is 2+2?"}]
+                  }]
+                })
+                .to_string(),
+            )
+            .build()
+            .wrap_err("failed to build request")?;
+
+        let response = client
+            .execute(request)
+            .await
+            .wrap_err("failed to send request")?;
+
+        // ensure response is ok
+        if !response.status().is_success() {
+            return Err(eyre!(
+                "Failed to make OpenAI chat request:\n{}",
+                response
+                    .text()
+                    .await
+                    .unwrap_or("Could not get error text as well".to_string())
+            ));
         }
+        log::debug!("Dummy request successful for model {}", model);
 
-        log::info!(
-            "Gemini checks are finished, using models: {:#?}",
-            available_models
-        );
-
-        Ok(available_models)
+        Ok(())
     }
 }
 
@@ -118,6 +184,8 @@ mod tests {
     async fn test_gemini_check() {
         let _ = dotenvy::dotenv(); // read api key
         assert!(env::var(ENV_VAR_NAME).is_ok(), "should have api key");
+        env::set_var("RUST_LOG", "none,dkn_workflows=debug");
+        let _ = env_logger::try_init();
 
         let models = vec![
             Model::Gemini10Pro,
--- workflows/src/providers/openai.rs
+++ workflows/src/providers/openai.rs
@@ -6,20 +6,7 @@ use std::env;
 
 use crate::utils::safe_read_env;
 
-const OPENAI_MODELS_API: &str = "https://api.openai.com/v1/models";
 const ENV_VAR_NAME: &str = "OPENAI_API_KEY";
-
-/// [Model](https://platform.openai.com/docs/api-reference/models/object) API object, fields omitted.
-#[derive(Debug, Clone, Deserialize)]
-struct OpenAIModel {
-    /// The model identifier, which can be referenced in the API endpoints.
-    id: String,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-struct OpenAIModelsResponse {
-    data: Vec<OpenAIModel>,
-}
 
 /// OpenAI-specific configurations.
 #[derive(Debug, Clone, Default)]
@@ -42,7 +29,7 @@ impl OpenAIConfig {
         self
     }
 
-    /// Check if requested models exist & are available in the OpenAI account.
+    /// Returns the list of model names available to this account.
     pub async fn check(&self, models: Vec<Model>) -> Result<Vec<Model>> {
         log::info!("Checking OpenAI requirements");
 
@@ -51,10 +38,62 @@ impl OpenAIConfig {
             return Err(eyre!("OpenAI API key not found"));
         };
 
-        // fetch models
+        // check if models exist within the account and select those that are available
+        let openai_model_names = self.fetch_models(&api_key).await?;
+        let mut available_models = Vec::new();
+        for requested_model in models {
+            // check if model exists
+            if !openai_model_names.contains(&requested_model.to_string()) {
+                log::warn!(
+                    "Model {} not found in your OpenAI account, ignoring it.",
+                    requested_model
+                );
+                continue;
+            }
+
+            // make a dummy request
+            if let Err(err) = self.dummy_request(&api_key, &requested_model).await {
+                log::warn!(
+                    "Model {} failed dummy request, ignoring it: {}",
+                    requested_model,
+                    err
+                );
+                continue;
+            }
+
+            available_models.push(requested_model)
+        }
+
+        // log results
+        if available_models.is_empty() {
+            log::warn!("OpenAI checks are finished, no available models found.",);
+        } else {
+            log::info!(
+                "OpenAI checks are finished, using models: {:#?}",
+                available_models
+            );
+        }
+
+        Ok(available_models)
+    }
+
+    /// Fetches the list of models available in the OpenAI account.
+    async fn fetch_models(&self, api_key: &str) -> Result<Vec<String>> {
+        /// [Model](https://platform.openai.com/docs/api-reference/models/object) API object, fields omitted.
+        #[derive(Debug, Clone, Deserialize)]
+        struct OpenAIModel {
+            /// The model identifier, which can be referenced in the API endpoints.
+            id: String,
+        }
+
+        #[derive(Debug, Clone, Deserialize)]
+        struct OpenAIModelsResponse {
+            data: Vec<OpenAIModel>,
+        }
+
         let client = Client::new();
         let request = client
-            .get(OPENAI_MODELS_API)
+            .get("https://api.openai.com/v1/models")
             .header("Authorization", format!("Bearer {}", api_key))
             .build()
             .wrap_err("failed to build request")?;
@@ -65,36 +104,61 @@ impl OpenAIConfig {
             .wrap_err("failed to send request")?;
 
         // parse response
-        if response.status().is_client_error() {
+        if !response.status().is_success() {
             return Err(eyre!(
                 "Failed to fetch OpenAI models:\n{}",
-                response.text().await.unwrap_or_default()
+                response
+                    .text()
+                    .await
+                    .unwrap_or("Could not get error text as well".to_string())
+            ));
+        } else {
+            let openai_models = response.json::<OpenAIModelsResponse>().await?;
+            Ok(openai_models.data.into_iter().map(|m| m.id).collect())
+        }
+    }
+
+    /// Makes a dummy request to the OpenAI API to check if the model is available & has credits.
+    async fn dummy_request(&self, api_key: &str, model: &Model) -> Result<()> {
+        log::debug!("Making a dummy request with: {}", model);
+        let client = Client::new();
+        let request = client
+            .post("https://api.openai.com/v1/chat/completions")
+            .header("Authorization", format!("Bearer {}", api_key))
+            .header("Content-Type", "application/json")
+            .body(
+                serde_json::json!({
+                  "model": model.to_string(),
+                  "messages": [
+                    {
+                      "role": "user",
+                      "content": "What is 2+2?"
+                    }
+                  ]
+                })
+                .to_string(),
+            )
+            .build()
+            .wrap_err("failed to build request")?;
+
+        let response = client
+            .execute(request)
+            .await
+            .wrap_err("failed to send request")?;
+
+        // ensure response is ok
+        if !response.status().is_success() {
+            return Err(eyre!(
+                "Failed to make OpenAI chat request:\n{}",
+                response
+                    .text()
+                    .await
+                    .unwrap_or("Could not get error text as well".to_string())
             ));
         }
-        let openai_models = response.json::<OpenAIModelsResponse>().await?;
+        log::debug!("Dummy request successful for model {}", model);
 
-        // check if models exist and select those that are available
-        let mut available_models = Vec::new();
-        for requested_model in models {
-            if !openai_models
-                .data
-                .iter()
-                .any(|m| m.id == requested_model.to_string())
-            {
-                log::warn!(
-                    "Model {} not found in your OpenAI account, ignoring it.",
-                    requested_model
-                );
-            } else {
-                available_models.push(requested_model);
-            }
-        }
-
-        log::info!(
-            "OpenAI checks are finished, using models: {:#?}",
-            available_models
-        );
-        Ok(available_models)
+        Ok(())
     }
 }
 
@@ -107,6 +171,8 @@ mod tests {
     async fn test_openai_check() {
         let _ = dotenvy::dotenv(); // read api key
         assert!(env::var(ENV_VAR_NAME).is_ok(), "should have api key");
+        env::set_var("RUST_LOG", "none,dkn_workflows=debug");
+        let _ = env_logger::try_init();
 
         let models = vec![Model::GPT4Turbo, Model::GPT4o, Model::GPT4oMini];
         let config = OpenAIConfig::new();
