67e3573f7010892bb2eea2e1dfb7b747a5643736
erhant <erhany96@gmail.com>
2025-02-04 10:34:13 +0300

    update network addresses

--- p2p/src/network.rs
+++ p2p/src/network.rs
@@ -45,10 +45,10 @@ impl DriaNetworkType {
     pub fn get_static_bootstrap_nodes(&self) -> Vec<Multiaddr> {
         match self {
              DriaNetworkType::Community => [
-                "/ip4/44.206.245.139/tcp/4001/p2p/16Uiu2HAm4q3LZU2T9kgjKK4ysy6KZYKLq8KiXQyae4RHdF7uqSt4",
-                "/ip4/18.234.39.91/tcp/4001/p2p/16Uiu2HAmJqegPzwuGKWzmb5m3RdSUJ7NhEGWB5jNCd3ca9zdQ9dU",
-                "/ip4/54.242.44.217/tcp/4001/p2p/16Uiu2HAmR2sAoh9F8jT9AZup9y79Mi6NEFVUbwRvahqtWamfabkz",
-                "/ip4/52.201.242.227/tcp/4001/p2p/16Uiu2HAmFEUCy1s1gjyHfc8jey4Wd9i5bSDnyFDbWTnbrF2J3KFb",
+                "/ip4/44.206.245.139/tcp/4001/p2p/16Uiu2HAmJjnAzHvjKMNLWN1ifPFsXkSXguzCkoxerZaF8gZYh5g6",
+                "/ip4/18.234.39.91/tcp/4001/p2p/16Uiu2HAkwm9ZNrVp2Td85YDoyYHaAn3UuRB2bnExM6muRYUQj6gL",
+                "/ip4/54.242.44.217/tcp/4001/p2p/16Uiu2HAmSGMTvisBqqwwMDdhywm9ZVeNerMiRH6Rz1UD4kSevgup",
+                "/ip4/52.201.242.227/tcp/4001/p2p/16Uiu2HAm54MS9d4zrtXffLxgz6dpDKjaXPVHyheXg4iMSvCBBtHX",
               //  "/ip4/18.205.158.27/tcp/4001/p2p/16Uiu2HAmB2GFG8oYa7DuivXYEsMUPiiKQ6yq9haQHRHHjJrn7FHo",
              ].iter(),
              DriaNetworkType::Pro => [].iter(),
@@ -63,10 +63,10 @@ impl DriaNetworkType {
     pub fn get_static_relay_nodes(&self) -> Vec<Multiaddr> {
         match self {
              DriaNetworkType::Community => [
-               "/ip4/34.201.33.141/tcp/4001/p2p/16Uiu2HAkuXiV2CQkC9eJgU6cMnJ9SMARa85FZ6miTkvn5fuHNufa",
-               "/ip4/18.232.93.227/tcp/4001/p2p/16Uiu2HAmHeGKhWkXTweHJTA97qwP81ww1W2ntGaebeZ25ikDhd4z",
-               "/ip4/54.157.219.194/tcp/4001/p2p/16Uiu2HAm7A5QVSy5FwrXAJdNNsdfNAcaYahEavyjnFouaEi22dcq",
-               "/ip4/54.88.171.104/tcp/4001/p2p/16Uiu2HAm5WP1J6bZC3aHxd7XCUumMt9txAystmbZSaMS2omHepXa",
+               "/ip4/34.201.33.141/tcp/4001/p2p/16Uiu2HAmKT8m2mTh9ciHWdY112VH7WvkqEBpZAic7ETSvso9XAsf",
+               "/ip4/18.232.93.227/tcp/4001/p2p/16Uiu2HAkzP6ZJuWJMRQdEzYapkvWGmycJkJTTvRhsePLox4DX4KU",
+               "/ip4/54.157.219.194/tcp/4001/p2p/16Uiu2HAmHEo5r8eD1m77E99JfKVb4qQd2gZv4wd2DySpxqdjcWc8",
+               "/ip4/54.88.171.104/tcp/4001/p2p/16Uiu2HAkvsLKDU5ufuU6DFsPfjkGSZh8qosLozWigR6WhJnCHgfU",
                // "/ip4/3.88.84.50/tcp/4001/p2p/16Uiu2HAmN35mw5MMf3SCUt3TpEU6WnBvjYJQ4ZZeKHRHEkSc4RPq",
              ].iter(),
              DriaNetworkType::Pro => [].iter(),
